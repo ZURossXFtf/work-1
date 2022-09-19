88e676dee57aeb4a3457cc4348e9905ef60c555f
vanish <vanish@DESKTOP-5NO7TTS>
2022-09-19 17:10:19 +0200

    Добавьте файлы проекта.

---
+++ work 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Захаров Иван Алексеевич 22-ИСП-2/2 Вариант 18
+//sin*sqrt(x-1) - sin*sqrt(x+1)
+
+
+try
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    double res = Math.Sin(Math.Sqrt(x + 1)) - Math.Sin(Math.Sqrt(x - 1));
+    Console.WriteLine($"{res:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
